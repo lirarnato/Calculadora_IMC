8a7e55defa9896399756ee05940076355e330498
lirar <lirarnato@hotmail.com>
2021-04-28 21:02:00 -0400

    Criação do layout da aplicação.

--- app/src/main/java/com/github/lirarnato/calculadora_imc/ResultActivity.kt
+++ app/src/main/java/com/github/lirarnato/calculadora_imc/ResultActivity.kt
@@ -3,11 +3,9 @@ package com.github.lirarnato.calculadora_imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_result)
     }
-
-
 }
